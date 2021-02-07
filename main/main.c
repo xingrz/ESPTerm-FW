0a6d187ce334c74b1757fe44ecd3baea7e5d701c
XiNGRZ <hi@xingrz.me>
2021-02-07 23:55:34 +0800

    Set up project

---
+++ main/main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+#include "esp_system.h"
+
+#define TAG "main"
+
+void app_main(void) {
+  ESP_LOGI(TAG, "SYSTEM READY");
+}
