5eddd49281a87cb00dcf0c140a1d2f3f3e0ebf1e
XiNGRZ <hi@xingrz.me>
2021-02-09 22:53:03 +0800

    Import LVGL v7

---
+++ components/lvgl/component.mk
@@ -0,0 +1,10 @@
+COMPONENT_SRCDIRS += lvgl/src/lv_core
+COMPONENT_SRCDIRS += lvgl/src/lv_hal
+COMPONENT_SRCDIRS += lvgl/src/lv_widgets
+COMPONENT_SRCDIRS += lvgl/src/lv_font
+COMPONENT_SRCDIRS += lvgl/src/lv_misc
+COMPONENT_SRCDIRS += lvgl/src/lv_themes
+COMPONENT_SRCDIRS += lvgl/src/lv_draw
+COMPONENT_SRCDIRS += lvgl/src/lv_gpu
+
+CFLAGS += -DLV_CONF_INCLUDE_SIMPLE
