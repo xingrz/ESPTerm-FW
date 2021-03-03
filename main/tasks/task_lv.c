6e3823184f3f23cdfb82554360cffd80e0474e1c
XiNGRZ <hi@xingrz.me>
2021-03-03 15:18:38 +0800

    Increase LVGL task handling interval

--- main/tasks/task_lv.c
+++ main/tasks/task_lv.c
@@ -23,7 +23,7 @@
 void lv_proc_task(void *arg) {
   while (1) {
     lv_task_handler();
-    vTaskDelay(10 / portTICK_PERIOD_MS);
+    vTaskDelay(30 / portTICK_PERIOD_MS);
   }
 
   vTaskDelete(NULL);
