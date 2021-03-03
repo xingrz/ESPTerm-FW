c38d344cae3692595760bb05e8dc559a1545d003
XiNGRZ <hi@xingrz.me>
2021-03-03 18:59:32 +0800

    Set up CI

--- main/component.mk
+++ main/component.mk
@@ -7,6 +7,6 @@ COMPONENT_SRCDIRS += readline
 COMPONENT_SRCDIRS += tasks
 COMPONENT_SRCDIRS += ui
 
-CFLAGS += -DGIT_TAG="\"$(shell git describe --tags --abbrev=0)\""
+CFLAGS += -DGIT_TAG="\"$(shell git describe --tags --abbrev=0 HEAD)\""
 CFLAGS += -DGIT_COMMIT="\"$(shell git rev-parse --short HEAD)\""
 CFLAGS += -DGIT_INCREMENT="\"$(shell git rev-list --count HEAD)\""
